1aee40846345b030f0d4c8f7ad185e4d01029048
Donisetti Ferreira Cosma <donisetti@msn.com>
2026-02-04 16:37:21 -0400

    Unificado

---
+++ Vasis.MDFe.Api/Controllers/HealthController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vasis.MDFe.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        /// <summary>
+        /// Endpoint de verificação de saúde da aplicação
+        /// </summary>
+        /// <returns>Status da aplicação e seus serviços</returns>
+        [HttpGet]
+        public IActionResult GetHealth()
+        {
+            var healthStatus = new
+            {
+                Status = "Healthy",
+                Timestamp = DateTime.UtcNow,
+                Version = "1.0.0",
+                Services = new
+                {
+                    ValidationService = "Active",
+                    LifecycleService = "Active"
+                }
+            };
+
+            return Ok(healthStatus);
+        }
+    }
+}
--- Vasis.MDFe.Api/Extensions/WebApplicationExtensions.cs
+++ Vasis.MDFe.Api/Extensions/WebApplicationExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using System;
 
 namespace Vasis.MDFe.Api.Extensions
 {
@@ -9,18 +8,6 @@ namespace Vasis.MDFe.Api.Extensions
     {
         public static WebApplication ConfigureRequestPipeline(this WebApplication app)
         {
-            //if (app.Environment.IsDevelopment())
-            //{
-            //    app.UseSwagger();
-            //    app.UseSwaggerUI(c =>
-            //    {
-            //        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Vasis MDFe API v1");
-            //        c.RoutePrefix = "swagger";
-            //    });
-            //}
-
-            // ***** ALTERAÇÃO AQUI: Habilitar o Swagger para todos os ambientes *****
-
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
@@ -28,11 +15,6 @@ namespace Vasis.MDFe.Api.Extensions
                 c.RoutePrefix = "swagger";
             });
 
-
-
-
-
-            // ***** CORREÇÃO AQUI: Desabilitar o redirecionamento HTTPS para o ambiente de testes *****
             // Verifica se a flag "DisableHttpsRedirectionForTests" está definida e é verdadeira
             var disableHttpsRedirectionForTests = app.Configuration.GetValue<bool>("DisableHttpsRedirectionForTests");
 
@@ -46,17 +28,6 @@ namespace Vasis.MDFe.Api.Extensions
             app.UseAuthentication();
             app.UseAuthorization();
             app.MapControllers();
-
-            app.MapGet("/health", () => new {
-                Status = "Healthy",
-                Timestamp = DateTime.UtcNow,
-                Version = "1.0.0",
-                Services = new
-                {
-                    ValidationService = "Active",
-                    LifecycleService = "Active"
-                }
-            });
 
             return app;
         }
