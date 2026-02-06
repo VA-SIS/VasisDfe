c238aa36cee832cf7dc99c6d46a98aa70674de44
Donisetti Ferreira Cosma <donisetti@msn.com>
2026-02-06 13:17:16 -0400

    correcao

--- Vasis.MDFe.Web/Vasis.MDFe.Web.Client/Program.cs
+++ Vasis.MDFe.Web/Vasis.MDFe.Web.Client/Program.cs
@@ -1,9 +1,21 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using Vasis.MDFe.Web.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Configurar HttpClient
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:7001/")
+});
+
+// Registrar Services
+builder.Services.AddScoped<MDFeService>();
+builder.Services.AddScoped<EventoService>();
+builder.Services.AddScoped<ConfiguracaoService>();
+
+// MudBlazor
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
