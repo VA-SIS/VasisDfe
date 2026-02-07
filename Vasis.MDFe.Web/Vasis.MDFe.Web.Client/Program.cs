bdc5ef90e4d18942d12ed6a04710ee4060e3aa37
Donisetti Ferreira Cosma <donisetti@msn.com>
2026-02-06 20:54:54 -0400

    Backup sem blazor

--- Vasis.MDFe.Web/Vasis.MDFe.Web.Client/Program.cs
+++ Vasis.MDFe.Web/Vasis.MDFe.Web.Client/Program.cs
@@ -1,19 +1,16 @@
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
-using Vasis.MDFe.Web.Client.Services;
+using Vasis.MDFe.Web.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Configurar HttpClient
 builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri("https://localhost:7001/")
 });
-
-// Registrar Services
-builder.Services.AddScoped<MDFeService>();
-builder.Services.AddScoped<EventoService>();
-builder.Services.AddScoped<ConfiguracaoService>();
 
 // MudBlazor
 builder.Services.AddMudServices();
